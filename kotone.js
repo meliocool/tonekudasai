f67e929b0738e42aac500f4b5f4f041337e8d4d8
melio <dhitanimam05@gmail.com>
2024-10-13 21:24:36 +0700

    popShipInitiated

---
+++ kotone.js
@@ -0,0 +1,38 @@
+const words = ["TONE KUDASAI", "KOTONE", "KAMIMOTO", "TRIPLES", "COTOCOLA"];
+        
+let currentIndex = 0;
+let currentText = '';
+let isDeleting = false;
+const typingSpeed = 100; 
+const deleteSpeed = 70;  
+const delayBetweenWords = 1000; 
+
+function RandomWord() {
+    return words[Math.floor(Math.random() * words.length)];
+}
+
+const h1Element = document.getElementById('dynamic-text');
+let fullText = RandomWord();
+
+function typeWords() {
+    
+    if (isDeleting) {
+        currentText = fullText.substring(0, currentText.length - 1);
+    } else {
+        currentText = fullText.substring(0, currentText.length + 1);
+    }
+
+    h1Element.textContent = currentText;
+
+    if (!isDeleting && currentText === fullText) {
+        setTimeout(() => isDeleting = true, delayBetweenWords); 
+    } else if (isDeleting && currentText === '') {
+        isDeleting = false;
+        fullText = RandomWord();
+    }
+
+    const speed = isDeleting ? deleteSpeed : typingSpeed;
+    setTimeout(typeWords, speed);
+}
+
+document.addEventListener('DOMContentLoaded', typeWords);
